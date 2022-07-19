c4abe353b8a6710e04d76de6126adc05ecd9b6b6
YaAnuar <42729761+YaAnuar@users.noreply.github.com>
2022-07-20 01:48:09 +0600

    frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import io from 'socket.io-client';
+
+import Table from "./TableContainer";
+import { SelectColumnFilter } from "./Filter";
+
+import "./App.css";
+
+const socket = io("http://127.0.0.1:2000/");
+
+function App() {
+  const [data, setData] = useState([]);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('get_data', (res) => {
+      console.log('pong')
+      setData(res);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('ping');
+  }
+  // useEffect(() => {
+  //   axios("http://localhost/api/table_data")
+  //     .then((res) => {
+  //       console.log(res.data)
+  //       setData(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  const columns = [
+    {
+      Header: "№ ",
+      accessor: "show.num",
+    },
+    {
+      Header: "Заказ №",
+      accessor: "show.order",
+    },
+    {
+      Header: "Стоимость,$",
+      accessor: "show.USD",
+    },
+    {
+      Header: "Срок поставки",
+      accessor: "show.time",
+    },
+    {
+      Header: "Стоимость в руб",
+      accessor: "show.RUB",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <h1>
+        <center>Таблица заказов</center>
+      </h1>
+      <Table columns={columns} data={data} />
+    </div>
+  );
+}
+
+export default App;
